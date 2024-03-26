c58638c60bb954be600e85be1883828095717568
Rohit More <more73629@gmail.com>
2024-03-26 11:39:41 +0530

    all files of todo project are intialized

---
+++ src/Component/Todos.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+const Todos = (props) => {
+  let myStyle={
+    minHeight:"70vh",
+    margin:"40px auto"
+
+  }
+  return (
+  
+     <div className="container " style={myStyle}>
+        <h3 className="text-center my-3">Todo List</h3>
+        {props.todos.length===0?"No to Display tosos Items":
+
+        props.todos.map((todo)=>{
+
+            return (
+              <>
+            
+            <TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}  /> 
+            <hr/>
+
+               </>
+            )
+            
+        
+
+      
+      })
+    }
+
+      </div>
+    
+  )
+}
+
+export default Todos
